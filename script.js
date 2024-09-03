861585c524f734ca2db7e203cc0ff94cc930aab6
mayestroga <mayestroga3@gmail.com>
2024-09-03 12:57:04 +0800

    changes

---
+++ script.js
@@ -0,0 +1,20 @@
+let menuIcon = document.querySelector('menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navLinks  = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec
+    })
+}
+
+
+
+
+
+menuIcon.onclick = () => {
+
+}
